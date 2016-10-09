434cd27ccc129ca5d705cfd4e0325927268c546c
luz karime baez galvis <luzkarimebaezgalvis@MBP-de-luz.home>
2016-10-09 00:11:24 -0500

    Testing

--- Prueba Token/ViewController.swift
+++ Prueba Token/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //Making a change to test github2
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
