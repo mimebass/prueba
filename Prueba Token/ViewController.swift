5600b9d8511e7ce0faa29bf1e9b88020977916ee
luz karime baez galvis <luzkarimebaezgalvis@MBP-de-luz.home>
2016-10-09 00:02:49 -0500

    Initial Commit

---
+++ Prueba Token/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba Token
+//
+//  Created by luz karime baez galvis on 9/10/16.
+//  Copyright (c) 2016 Kew-Baez Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Prueba TokenTests/Prueba_TokenTests.swift
@@ -0,0 +1,36 @@
+//
+//  Prueba_TokenTests.swift
+//  Prueba TokenTests
+//
+//  Created by luz karime baez galvis on 9/10/16.
+//  Copyright (c) 2016 Kew-Baez Media. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Prueba_TokenTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
